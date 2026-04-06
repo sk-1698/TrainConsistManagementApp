8a3147d9ebe4cd6bdd00ed178821d0bcc2021916
sk-1698 <sk1698@srmist.edu.in>
2026-04-06 15:22:56 +0530

    UC7: Sort bogies by capacity using Comparator

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,18 +1,41 @@
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class TrainConsistManagementApp {
+    static class Bogie {
+        String name;
+        int capacity;
 
-    public static void main(String[] args) {
-
-        HashMap<String, Integer> capacityMap = new HashMap<>();
-        capacityMap.put("Sleeper", 72);
-        capacityMap.put("AC Chair", 60);
-        capacityMap.put("First Class", 40);
-        System.out.println("\nBogie Capacity Details:");
-        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
         }
+
+        @Override
+        public String toString() {
+            return name + " -> " + capacity;
+        }
+    }
+    public static void main(String[] args) {
+        // ================= UC7 START =================
+
+// Create list of bogies
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
+
+// Sort by capacity
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+// Display sorted bogies
+        System.out.println("\nBogies sorted by capacity:");
+
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+
+// ================= UC7 END =================
     }
 }
